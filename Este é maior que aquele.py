fd29f49186f05d40682bd298b0400a7dbc7dc07a
Arthur Lima <150402860+ArthurLimaTec@users.noreply.github.com>
2024-02-04 21:20:06 +0000

    Primeiro Exercício

---
+++ Este é maior que aquele.py
@@ -0,0 +1,11 @@
+#O VALOR MAIOR DEVE SEMPRE APARECER PRIMEIRO
+
+primeiro_valor = input('Digite um valor:')
+segundo_valor = input('Digite outro valor:')
+
+if primeiro_valor > segundo_valor:
+    print(primeiro_valor, 'É maior que', segundo_valor)
+
+elif segundo_valor > primeiro_valor:
+    print(segundo_valor, 'É maior que', primeiro_valor)
+
